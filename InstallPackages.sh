2e4e749b07174d5738e89587833ea5719c2af48c
eske4 <36681234+eske4@users.noreply.github.com>
2023-10-31 06:10:15 +0100

    Update InstallPackages.sh
    Updated to with mains features

--- InstallPackages.sh
+++ InstallPackages.sh
@@ -64,25 +64,38 @@ custom_packages_yay=(
     code-marketplace
 )
 
-# ANSI escape codes for bright pink text
+
+# ANSI escape codes for colored text
 BRIGHT_PINK='\e[95m'
 RESET_COLOR='\e[0m'
 
-# Function to echo with bright pink color
-pink_echo() {
-    echo -e "${BRIGHT_PINK}$1${RESET_COLOR}"
+# Function to echo with colored text
+colored_echo() {
+    local color="$1"
+    local message="$2"
+    echo -e "${color}$message${RESET_COLOR}"
 }
 
 # Function to install packages with the specified package manager (pacman or yay)
 install_packages() {
-    local package_manager=$1
+    local package_manager="$1"
     shift
     local packages=("$@")
-    pink_echo "Installing packages using $package_manager"
+    colored_echo "$BRIGHT_PINK" "Installing packages using $package_manager"
     if [ "$package_manager" == "yay" ]; then
         yay -S --noconfirm "${packages[@]}"
     else
         sudo pacman -S --noconfirm "${packages[@]}"
+    fi
+}
+
+# Function to install microcode if desired
+install_microcode() {
+    local cpu_type="$1"
+    read -p "Do you have an $cpu_type CPU? (yes[Y]/no[N]): " response
+    response="${response,,}"
+    if [[ "$response" == "yes" || "$response" == "y" ]]; then
+        install_packages "pacman" "$cpu_type-ucode"
     fi
 }
 
@@ -102,27 +115,27 @@ sudo pacman -Sy --noconfirm
 
 # Install essential packages using pacman
 install_packages "pacman" "${essential_packages[@]}"
-pink_echo "Installing custom packages for pacman"
+colored_echo "$BRIGHT_PINK" "Installing custom packages for pacman"
 install_packages "pacman" "${custom_packages_pacman[@]}"
 
 # Load Bluetooth module and enable the service
-pink_echo "Setting up Bluetooth..."
+colored_echo "$BRIGHT_PINK" "Setting up Bluetooth..."
 sudo modprobe btusb
 sudo systemctl enable --now bluetooth
 
 # Install yay using git
-pink_echo "Installing yay package manager"
+colored_echo "$BRIGHT_PINK" "Installing yay package manager"
 yay_dir="/tmp/yay"
 git clone https://aur.archlinux.org/yay.git "$yay_dir"
 cd "$yay_dir" || exit
 makepkg -si --noconfirm
 
 # Install AUR packages using yay
-pink_echo "Installing essentials through yay"
+colored_echo "$BRIGHT_PINK" "Installing essentials through yay"
 install_packages "yay" "${aur_packages[@]}"
 
 # Install custom packages using yay
-pink_echo "Installing custom packages for yay"
+colored_echo "$BRIGHT_PINK" "Installing custom packages for yay"
 install_packages "yay" "${custom_packages_yay[@]}"
 
 # Remove downloaded files
@@ -133,32 +146,26 @@ read -p "Do you want to set up a C++ development environment? (yes[Y]/no[N]): " 
 response="${response,,}"  # Convert to lowercase for case-insensitive comparison
 if [[ "$response" == "yes" || "$response" == "y" ]]; then
     install_packages "pacman" "${cplusplus_packages[@]}"
-    pink_echo "C++ development environment set up."
+    colored_echo "$BRIGHT_PINK" "C++ development environment set up."
 else
-    pink_echo "C++ development environment not set up."
+    colored_echo "$BRIGHT_PINK" "C++ development environment not set up."
 fi
 
-# Install microcode for Intel CPUs if desired
-read -p "Do you have an Intel CPU? (yes[Y]/no[N]): " response
-response="${response,,}"
-if [[ "$response" == "yes" || "$response" == "y" ]]; then
-    install_packages "pacman" "intel-ucode"
-fi
-
-# Install microcode for AMD CPUs if desired
-read -p "Do you have an AMD CPU? (yes[Y]/no[N]): " response
-response="${response,,}"
-if [[ "$response" == "yes" || "$response" == "y" ]]; then
-    install_packages "pacman" "amd-ucode"
-fi
+# Install microcode for Intel and AMD CPUs if desired
+install_microcode "intel"
+install_microcode "amd"
 
 cd "$current_dir"
 
+# Adding Guake to autostart
+colored_echo "$BRIGHT_PINK" "Adding Guake to autostart"
 sudo cp /usr/share/guake/autostart-guake.desktop /etc/xdg/autostart/autostart-guake.desktop
 
 # Update GRUB configuration
+colored_echo "$BRIGHT_PINK" "Updating GRUB configuration"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable UFW and Preload services
+colored_echo "$BRIGHT_PINK" "Enabling UFW and Preload services"
 sudo systemctl enable --now ufw
 sudo systemctl enable --now preload
